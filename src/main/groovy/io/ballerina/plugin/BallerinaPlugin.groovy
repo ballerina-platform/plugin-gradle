5b57b170c9e5b862d57bc485a839bff51730a758
niveathika <niveathika@gmail.com>
2021-06-02 12:48:50 +0530

    Revert "Update default code coverage to use package organisation"
    This reverts commit 9b4a1a60

--- src/main/groovy/io/ballerina/plugin/BallerinaPlugin.groovy
+++ src/main/groovy/io/ballerina/plugin/BallerinaPlugin.groovy
@@ -173,7 +173,7 @@ class BallerinaPlugin implements Plugin<Project> {
                 }
                 if (graph.hasTask(":${packageName}-ballerina:test")) {
                     if (project.extensions.ballerina.testCoverageParam == null) {
-                        testParams = "--code-coverage --coverage-format=xml --includes=org.ballerinalang.stdlib.${packageName}.*:${packageOrg}.${packageName}.*"
+                        testParams = "--code-coverage --coverage-format=xml --includes=org.ballerinalang.stdlib.${packageName}.*:ballerina.${packageName}.*"
                     } else {
                         testParams = project.extensions.ballerina.testCoverageParam
                     }
